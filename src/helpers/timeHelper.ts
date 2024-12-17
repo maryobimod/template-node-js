fb39439ddf0fd402691c5f99218e3b335d55f93c
aryobimo14 <muhammadaryo60@gmail.com>
2024-12-17 15:08:12 +0700

    Perbaikan getCurrentWIBDate() di helpers

--- src/helpers/timeHelper.ts
+++ src/helpers/timeHelper.ts
@@ -1,25 +1,10 @@
 // src/helpers/timeHelper.ts
 
-import moment from "moment-timezone";
-
-/**
- * Mendapatkan waktu sekarang dalam format string di zona waktu WIB
- * @param format Format waktu, default: ISO-8601 ("YYYY-MM-DDTHH:mm:ss.SSSZ")
- * @returns {string} Waktu dalam format string
- */
-export const getCurrentWIBDate = (
-  format = "YYYY-MM-DDTHH:mm:ss.SSSZ"
-): string => {
-  return moment().tz("Asia/Jakarta").format(format);
-};
-
-/**
- * Mendapatkan waktu sekarang dalam format string di zona waktu WIB
- * @param format Format waktu, default: "YYYY-MM-DD HH:mm:ss"
- * @returns {string} Waktu dalam format string
- */
-export const getCurrentWIBFormatted = (
-  format = "YYYY-MM-DD HH:mm:ss"
-): string => {
-  return moment().tz("Asia/Jakarta").format(format);
+export const getCurrentWIBDate = (): Date => {
+  const localDate = new Date();
+  const jakartaTimezoneOffset = 7 * 60; // Jakarta is UTC+7
+  const jakartaDate = new Date(
+    localDate.getTime() + jakartaTimezoneOffset * 60 * 1000
+  );
+  return jakartaDate;
 };
